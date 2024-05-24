d98422666ddc946cbc36ba1f937f5e24075de326
Kisoo123 <167399086+Kisoo123@users.noreply.github.com>
2024-05-24 12:27:01 +0900

    KS 파일 수정

--- SemiProject-KLP/src/main/java/com/kupid/member/log/controller/LoginServlet.java
+++ SemiProject-KLP/src/main/java/com/kupid/member/log/controller/LoginServlet.java
@@ -8,9 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.kupid.member.model.dto.MemberDto;
-import com.kupid.mypage.service.MyPageService;
-
 /**
  * Servlet implementation class MyProfileServlet
  */
