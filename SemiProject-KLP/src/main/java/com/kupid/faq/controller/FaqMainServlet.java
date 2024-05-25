bfc6a2b1e1e84aa66a5c778efecf110ae27bb757
Hyejeong Kim <heajung2665@naver.com>
2024-05-26 05:21:28 +0900

    Merge branch 'dev' of https://github.com/Kisoo123/Kupid.git into hj faq 리스트 불러오기,페이징 처리 완료함 검색 기능 ajax 써보려다실패 고객센터,faq,문의하기전체남은기능2개 모두 ajax사용

--- SemiProject-KLP/src/main/java/com/kupid/faq/controller/FaqMainServlet.java
+++ SemiProject-KLP/src/main/java/com/kupid/faq/controller/FaqMainServlet.java
@@ -1,11 +1,31 @@
 package com.kupid.faq.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kupid.faq.model.dto.FaqDto;
+import com.kupid.faq.model.service.FaqSevice;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kupid.faq.model.dto.FaqDto;
+import com.kupid.faq.model.service.FaqSevice;
+import com.kupid.faq.model.dto.FaqDto;
+
+
 
 /**
  * Servlet implementation class FaqMainServlet
@@ -26,7 +46,69 @@ public class FaqMainServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher(getServletContext().getInitParameter("viewpath")+"faq/faq.jsp").forward(request, response);
+		
+		request.setAttribute("pagenum", 3);
+		int cPage=1;
+		try {
+			cPage=Integer.parseInt(request.getParameter("cPage"));
+		}catch(NumberFormatException e){
+			
+		}
+		int numBerpage=5;
+		try {
+			numBerpage=Integer.parseInt(request.getParameter("numBerpage"));
+		}catch(NumberFormatException e){
+			
+		}
+		
+		int totalData=new FaqSevice().selectAllCount();
+		int totalPage=(int)Math.ceil((double)totalData/numBerpage);
+		int pageBarSize=5;
+		int pageNo=((cPage-1)/pageBarSize)*pageBarSize+1;
+		int pageEnd=pageNo+pageBarSize-1;
+		
+		StringBuffer sb=new StringBuffer();
+		sb.append("<ul class='pagination justify-content-center'>");
+		
+		if(pageNo==1) {
+			sb.append("<li class='page-item'>");
+			sb.append("<a href='#'>이전</a>");
+			sb.append("</li>");
+		}else {
+			sb.append("<li class='page-item'>");
+			sb.append("<a href='"+request.getRequestURI()+"?cPage="+(pageNo-1)+"&numBerpage="+numBerpage+"'>이전</a>");
+			sb.append("</li>");
+		}
+		
+		while(!(pageNo>pageEnd||pageNo>totalPage)) {
+			if(pageNo==cPage) {
+				sb.append("<li class='page-item'>");
+				sb.append("<a href='#'>"+pageNo+"</a>");
+				sb.append("</li>");
+			}else {
+				sb.append("<li class='page-item'>");
+				sb.append("<a href='"+request.getRequestURI()+"?cPage="+(pageNo)+"&numBerpage="+numBerpage+"'>"+pageNo+"</a>");
+				sb.append("</li>");
+			}
+			pageNo++;
+		}
+		
+		if(pageNo>totalPage) {
+			sb.append("<li class='page-item'>");
+			sb.append("<a href='#'>다음</a>");
+			sb.append("</li>");
+		}else {
+			sb.append("<li class='page-item'>");
+			sb.append("<a href='"+request.getRequestURI()+"?cPage="+(pageNo)+"&numBerpage="+numBerpage+"'>다음</a>");
+			sb.append("</li>");
+		}
+		sb.append("</ul>");
+		request.setAttribute("pageBar", sb);
+		
+		
+		List<FaqDto> faq=new FaqSevice().selectFaqAll(cPage, numBerpage);
+		request.setAttribute("faq", faq);
+		request.getRequestDispatcher("/WEB-INF/views/faq/faq.jsp").forward(request, response);
 	}
 
 	/**
--- SemiProject-KLP/src/main/webapp/WEB-INF/views/customer/customer.jsp
+++ SemiProject-KLP/src/main/webapp/WEB-INF/views/customer/customer.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ page import="java.util.List,com.kupid.faq.model.dto.Faq,java.text.SimpleDateFormat"%>     
+<%@ page import="java.util.List,com.kupid.faq.model.dto.FaqDto,java.text.SimpleDateFormat"%>     
 <%@ include file="/WEB-INF/views/common/header.jsp" %>  
 
     <title>kupid 고객센터</title>
